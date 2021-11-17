8eed35a29085f1360617c8012989064527a87b97
fujimoto kosuke <dorama.1995@gmail.com>
2021-11-18 01:03:36 +0900

    チャットボット実装

--- App.js
+++ App.js
@@ -1,13 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import React, { useState, useCallback, useEffect } from 'react';
+import uuid from 'react-native-uuid';
+
+const CHAT_URL = 'https://{QnA-Maker-endpoint}/knowledgebases/{knowledge-base-ID}/generateAnswer'
+const ENDPOINT_KEY = 'EndpointKey {QnA Makerのエンドポイントキー}'
+
+// 利用者(右)側のユーザー
+const USER = {
+  _id: 1,
+  name: 'Me',
+}
+// ボット(左)側のユーザー
+const BOT = {
+  _id: 2,
+  name: 'Bot',
+  avatar: 'https://placeimg.com/140/140/any',
+}
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    setMessages([
+      {
+        _id: uuid.v4(),
+        text: '質問はありますか？',
+        createdAt: new Date(),
+        user: BOT,
+      },
+    ])
+  }, [])
+
+  /* QnA MakerのgenerateAnswerAPIをコール */
+  const generateAnswer = async (message) => {
+    try {
+     const response = await fetch(CHAT_URL, {
+      method: 'POST',
+      headers: {
+        Authorization: ENDPOINT_KEY,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        question: message,
+      })
+    })
+     const json = await response.json();
+     const responseMessages = {
+       _id: uuid.v4(),
+       text: json.answers[0].answer,
+       createdAt: new Date(),
+       user: BOT,
+     }
+     /* レスポンスをmessagesに追加 */
+     setMessages(previousMessages => GiftedChat.append(previousMessages, responseMessages))
+    } catch (error) {
+     console.error(error);
+   }
+ }
+
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+    generateAnswer(messages[0].text);
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={USER}
+    />
   );
 }
 
